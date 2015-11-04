74d907cbe0c33c9b130f3d0cb50ac924bca6733a
Sergio Azevedo <sergio.azevedo@corp.globo.com>
2015-11-04 17:49:37 -0200

    adds react component example

--- source/app.js
+++ source/app.js
@@ -7,7 +7,49 @@ var listOfItems = <ul className="list-of-items">
                     <li className="item-3">Item 3</li>
                   </ul>;
 
+
+var ReactClass = React.createClass({
+  getInitialState: function(){
+    return {
+      isHeaderHidden: false
+    };
+  },
+
+  handleClick: function(){
+    this.setState({
+      isHeaderHidden: !this.state.isHeaderHidden
+    });
+  },
+
+  render: function(){
+    var title: 'Stateful React Component';
+    var headerElement = React.createElement('h1',
+      {
+        className: 'header',
+        key: 'header'
+      },
+      this.state.title
+    ); 
+
+    var buttonElement = React.createElement('button',
+      {
+        className: 'btn btn-default',
+        onClick: this.handleClick,
+        key: 'button'
+      },
+      'Toggle header'
+    );
+
+    if (this.state.isHeaderHidden){
+      return React.createElement('div',null, [buttonElement]);
+    }
+    return React.createElement('div', null, [buttonElement, headerElement]);
+  }
+})
+
+var element = React.createElement(ReactClass)
+
 ReactDOM.render(
-  listOfItems,
+  element,
   document.getElementById('react-application')
 );
