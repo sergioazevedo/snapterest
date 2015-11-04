5ee9e8aefea937cb4499745d129a08a91d317576
sergioazevedo <sergior.jr@gmail.com>
2015-11-04 18:52:34 -0200

    adds Application component

--- source/app.js
+++ source/app.js
@@ -1,55 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var listOfItems = <ul className="list-of-items">
-                    <li className="item-1">Item 1</li>
-                    <li className="item-2">Item 2</li>
-                    <li className="item-3">Item 3</li>
-                  </ul>;
-
-
-var ReactClass = React.createClass({
-  getInitialState: function(){
-    return {
-      isHeaderHidden: false
-    };
-  },
-
-  handleClick: function(){
-    this.setState({
-      isHeaderHidden: !this.state.isHeaderHidden
-    });
-  },
-
-  render: function(){
-    var title: 'Stateful React Component';
-    var headerElement = React.createElement('h1',
-      {
-        className: 'header',
-        key: 'header'
-      },
-      this.state.title
-    ); 
-
-    var buttonElement = React.createElement('button',
-      {
-        className: 'btn btn-default',
-        onClick: this.handleClick,
-        key: 'button'
-      },
-      'Toggle header'
-    );
-
-    if (this.state.isHeaderHidden){
-      return React.createElement('div',null, [buttonElement]);
-    }
-    return React.createElement('div', null, [buttonElement, headerElement]);
-  }
-})
-
-var element = React.createElement(ReactClass)
+var Application = require('./components/Application.react');
 
 ReactDOM.render(
-  element,
+  <Application />, 
   document.getElementById('react-application')
 );
